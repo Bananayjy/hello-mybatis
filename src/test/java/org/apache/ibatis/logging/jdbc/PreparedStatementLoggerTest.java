b84e0d4b9349dd23e12dc33f0ca9862e55c70130
banana <2322092442@qq.com>
2025-03-30 14:56:18 +0800

    logic: 日志模块注释

--- src/test/java/org/apache/ibatis/logging/jdbc/PreparedStatementLoggerTest.java
+++ src/test/java/org/apache/ibatis/logging/jdbc/PreparedStatementLoggerTest.java
@@ -50,34 +50,46 @@ class PreparedStatementLoggerTest {
 
   @BeforeEach
   void setUp() throws SQLException {
+    // 通过Mock生成的模拟对象，创建PreparedStatement对象
     ps = PreparedStatementLogger.newInstance(this.preparedStatement, log, 1);
   }
 
   @Test
   void shouldPrintParameters() throws SQLException {
+    // 当调用log的isDebugEnabled时，返回true，即开启dubug日志打印
     when(log.isDebugEnabled()).thenReturn(true);
+    // 当调用preparedStatement的executeQuery时，返回resultSet对象，即模拟查询结果
     when(preparedStatement.executeQuery(anyString())).thenReturn(resultSet);
 
+    // 通过preparedStatement对象执行查询操作
     ps.setInt(1, 10);
     ResultSet rs = ps.executeQuery("select 1 limit ?");
 
+    // 判断log是否调用dubug方法，并且参数包含Parameters: 10(Integer)
     verify(log).debug(contains("Parameters: 10(Integer)"));
+    // 判断rs是否为空，并且rs是否与resultSet相同，如果相同，则说明执行查询操作失败
     Assertions.assertNotNull(rs);
     Assertions.assertNotSame(resultSet, rs);
   }
 
   @Test
   void shouldPrintNullParameters() throws SQLException {
+    // 当调用log的isDebugEnabled时，返回true，即开启dubug日志打印
     when(log.isDebugEnabled()).thenReturn(true);
+    // 当调用preparedStatement的execute时，返回true，即模拟执行成功
     when(preparedStatement.execute(anyString())).thenReturn(true);
 
+    // 执行操作
     ps.setNull(1, JdbcType.VARCHAR.TYPE_CODE);
     boolean result = ps.execute("update name = ? from test");
 
+    // 看log是否调用debug方法，并且参数为null
     verify(log).debug(contains("Parameters: null"));
+    // 判断执行结果是否为true，如果为true，则说明执行成功
     Assertions.assertTrue(result);
   }
 
+  // 验证当调用 getResultSet() 和 getParameterMetaData() 方法时，PreparedStatementLogger 不会触发 log.debug() 日志记录。
   @Test
   void shouldNotPrintLog() throws SQLException {
     ps.getResultSet();
@@ -88,11 +100,15 @@ class PreparedStatementLoggerTest {
 
   @Test
   void shouldPrintUpdateCount() throws SQLException {
+    // 当调用log的isDebugEnabled时，返回true，即开启dubug日志打印
     when(log.isDebugEnabled()).thenReturn(true);
+    // 当调用preparedStatement的getUpdateCount时，返回1，即模拟更新操作
     when(preparedStatement.getUpdateCount()).thenReturn(1);
 
+    // 调用getUpdateCount方法
     ps.getUpdateCount();
 
+    // 验证log是否调用了debug方法，并且参数包含Updates: 1
     verify(log).debug(contains("Updates: 1"));
   }
 }
